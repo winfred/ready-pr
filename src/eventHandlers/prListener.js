3ecbf2b8c605d7c20f0e9b145af859c3a3fd514f
Winfred Nadeau <winfred.nadeau@pm.me>
2021-06-25 15:30:55 -0500

    init

---
+++ src/eventHandlers/prListener.js
@@ -0,0 +1,16 @@
+
+aha.on({ event: 'aha-develop.github.pr.labeled' }, async ({record, payload}, { identifier, settings }) => {
+  if (payload.label.name = 'Ready') {
+    record.workflowStatus = { name: 'Ready to ship' };
+    record.teamWorkflowStatus = { name: 'Awaiting Deployment' };
+    await record.save();
+  }
+});
+
+aha.on({ event: 'aha-develop.github.pr.unlabeled' }, async ({ record, payload }, { identifier, settings }) => {
+  if (payload.label.name = 'Ready') {
+    record.workflowStatus = { name: 'In development' };
+    record.teamWorkflowStatus = { name: 'In progress' };
+    await record.save();
+  }
+});
